87c8032537ac46d4c53238958825f824fd6e2caa
marszzibros <74686781+marszzibros@users.noreply.github.com>
2025-01-24 02:06:02 -0500

    change immed to 31 microprocessor.py

--- microprocessor.py
+++ microprocessor.py
@@ -10,11 +10,11 @@ class Instruction:
 
 
 class CPU:
-    def __init__(self, pc, next_pc, memory, registers):
+    def __init__(self, pc, memory_size, num_registers):
         self.pc = pc
-        self.next_pc = next_pc
-        self.memory = np.zeros(memory, dtype=int)
-        self.registers = np.zeros(registers, dtype=int)
+        self.next_pc = None
+        self.memory = np.zeros(memory_size, dtype=int)
+        self.registers = np.zeros(num_registers, dtype=int)
 
     def fetch(self):
         # Read the instruction at location mem[pc]
@@ -32,32 +32,60 @@ class CPU:
         Rd = (instruction >> 24) & 15
         Rs1 = (instruction >> 20) & 15
         Rs2 = (instruction >> 16) & 15
-        immed = instruction & 15
+        immed = instruction & 31
         
         # create an instance of Instruction
         return Instruction(opcode, Rd, Rs1, Rs2, immed)
     
     def execute(self, instruction):
+        # NOOP
         if instruction.opcode == 0:
-            pass 
-        elif instruction.opcode == 1:
-            self.registers[instruction.Rd] = self.registers[instruction.Rs1] + self.registers[instruction.Rs2]
-        elif instruction.opcode == 2:
-            self.registers[instruction.Rd] = self.registers[instruction.Rs1]
+            pass
+
+        # ADD and ADDI 
+        elif instruction.opcode == 1 or instruction.opcode == 2:
+            if instruction.opcode == 1:
+                
+                alu_result = self.registers[instruction.Rs1] + self.registers[instruction.Rs2]
+            elif instruction.opcode == 2:
+                alu_result = self.registers[instruction.Rs1] + instruction.immed
+
+            if instruction.Rd != 0:
+                self.registers[instruction.Rd] = alu_result
+            
+        # BEQ
         elif instruction.opcode == 3:
             if self.registers[instruction.Rs1] == self.registers[instruction.Rs2]:
                 self.next_pc = self.pc + instruction.immed
             else:
                 pass
+        
+        # JAL
         elif instruction.opcode == 4:
-            pass
+            alu_result = self.pc + 1
+            if instruction.Rd != 0:
+                self.registers[instruction.Rd] = alu_result
+            self.next_pc = self.pc + instruction.immed
+
+        # LW
         elif instruction.opcode == 5:
-            pass
+            eff_address = self.registers[instruction.Rs1] + instruction.immed
+            if instruction.Rd != 0:
+                self.registers[instruction.Rd] = self.memory[eff_address]
+    
+        # SW
         elif instruction.opcode == 6:
+            eff_address = self.registers[instruction.Rs2] + instruction.immed
+            self.memory[eff_address] = self.registers[instruction.Rs1]
             pass
+        
+        # RETURN
         elif instruction.opcode == 7:
-            pass
+            return 1
+        
         else:
-            print("Invalid opcode")
+            raise("Invalid opcode")
+        
+        return 0
 
     
