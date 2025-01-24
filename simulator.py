582e852f7c73a1842d3b8ee0a8543901297cac0b
marszzibros <74686781+marszzibros@users.noreply.github.com>
2025-01-24 02:06:27 -0500

    tester roughly done

--- simulator.py
+++ simulator.py
@@ -1,28 +1,29 @@
 import numpy as np
 from microprocessor import CPU, Instruction
+from data_utils import read_instruction, parse_instruction
 
 MEM_SIZE = 65536
 NUM_REGISTERS = 16
 
-# A function that builds insturction
-def build_instruction(opcode, Rd, Rs1, Rs2, immed):
-    instr = opcode << 28
-    if Rd is not None:
-        instr = instr + (Rd << 24)
-    if Rs1 is not None:
-        instr = instr + (Rs1 << 20)
-    if Rs2 is not None:
-        instr = instr + (Rs2 << 16)
-    if immed is not None:
-        instr = instr + immed
-    return instr
+instruction_path = "instruction.txt"
 
-# Assign constant values to represent opcodes
-opcode_dict = { "NOOP"  : 0,    
-                "ADD"   : 1,   
-                "ADDI"  : 2,    
-                "BEQ"   : 3,
-                "JAL"   : 4,
-                "LW"    : 5,
-                "SW"    : 6,
-                "RETURN": 7}
+instructions = []
+pc = 100
+cpu = CPU(pc, MEM_SIZE, NUM_REGISTERS)
+
+for index, line in enumerate(read_instruction(instruction_path)):
+    instruction = parse_instruction(line)
+
+    cpu.memory[pc + index] = instruction
+
+while True:
+    instruction = cpu.fetch()
+    instruction = cpu.decode(instruction)
+
+    status = cpu.execute(instruction)
+    if status == 1:
+        break
+    cpu.pc = cpu.next_pc
+
+print(cpu.registers)
+print(cpu.memory[26])
